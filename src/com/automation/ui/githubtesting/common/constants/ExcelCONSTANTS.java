f0ed8e1d92243e41cc34320096e94ef16eae677c
Saju K K <Saju K K>
2019-08-12 13:50:14 +0530

    Deleted the unwanted

--- src/com/automation/ui/githubtesting/common/constants/ExcelCONSTANTS.java
+++
@@ -1,7 +0,0 @@
-package com.automation.ui.githubtesting.common.constants;
-
-public interface ExcelCONSTANTS {
-    public String LOGINXLSDATAPATH = "resources/data/githubdataselenium.xls";
-    public String LOGINGITHUB = "GITHUBTEST";
-
-}
