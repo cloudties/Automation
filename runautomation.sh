3e580d51eada257a9e4853f7580ac810b1dae83b
Saju K K <sajukk@ai-robots.co>
2023-01-14 15:56:20 +0530

    script updated

--- runautomation.sh
+++ runautomation.sh
@@ -1,15 +1,19 @@
 #bin/bash
 
-cd /Users/e12390/IdeaProjects/Automation
+cd /opt/software/Automation
 
 echo  ------------- KILL ALL EXISTING BROWSER INSTANCES----------------------
 killall chromerun chromerun
 killall firefox
+killall chrome
+killall geckodriver
+
 
 
 echo "pulling code from git"
 
-cd /Users/e12390/IdeaProjects/Automation
+cd /opt/software/Automation
+
 git pull
 export JAVA_HOME=$(/usr/libexec/java_home)
 export PATH=/opt/homebrew/bin:$PATH
